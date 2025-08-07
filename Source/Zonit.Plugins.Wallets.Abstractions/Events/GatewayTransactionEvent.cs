7c2944be85c5c44d6be61799ace562e99e74c930
Alerinos <42449535+Alerinos@users.noreply.github.com>
2025-08-07 10:23:32 +0200

    Zaktualizowano wersję Assembly i dodano nowy projekt
    Zaktualizowano wersję zestawu Assembly w pliku `Directory.Packages.props` z `0.0.6` na `0.0.8`.
    Dodano nowy projekt `Zonit.Plugins.Wallets.Abstractions` do rozwiązania, w tym jego konfigurację w sekcjach `ProjectConfigurationPlatforms` oraz `NestedProjects`.
    W pliku `Zonit.Plugins.Wallets.Abstractions.csproj` dodano nową definicję projektu z wieloma docelowymi frameworkami (`net8.0`, `net9.0`, `net10.0`).
    Wprowadzono nową klasę `GatewayTransactionEvent` w pliku `GatewayTransactionEvent.cs`, która reprezentuje zdarzenie transakcji przetwarzanej przez bramkę płatności, z wymaganymi właściwościami takimi jak `Id`, `DepositId`, `Amount`, `Currency` oraz `Status`.

---
+++ Source/Zonit.Plugins.Wallets.Abstractions/Events/GatewayTransactionEvent.cs
@@ -0,0 +1,23 @@
+﻿namespace Zonit.Plugins.Wallets.Abstractions;
+
+/// <summary>
+/// Event that represents a transaction processed through a payment gateway.
+/// </summary>
+public class GatewayTransactionEvent
+{
+    public required Guid Id { get; set; }
+    public required Guid DepositId { get; set; }
+    public Guid? OrganizationId { get; set; }
+    public Guid? UserId { get; set; }
+
+    /// <summary>
+    /// The amount of the transaction, represented as a decimal value.
+    /// </summary>
+    public required decimal Amount { get; set; }
+
+    /// <summary>
+    /// Gets or sets the ISO 4217 currency code associated with the transaction or amount.
+    /// </summary>
+    public required string Currency { get; set; }
+    public required string Status { get; set; }
+}
